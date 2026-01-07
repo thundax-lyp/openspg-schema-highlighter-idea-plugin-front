2b949239b1f081153654eb50e48ec349f1d474e1
thundax-lyp <731378491@qq.com>
2026-01-07 10:20:40 +0800

    refactor

--- src/pages/schema-diagram/components/icons/index.tsx
+++ src/pages/schema-diagram/components/icons/index.tsx
@@ -1,14 +1,14 @@
-export * from "./actions/DownloadIcon"
+export * from "./actions/download-icon"
 
-export * from "./nodes/PropertyIcon"
-export * from "./nodes/RelationIcon"
+export * from "./nodes/property-icon"
+export * from "./nodes/relation-icon"
 
-export * from "./types/ConceptIcon"
-export * from "./types/EntityIcon"
-export * from "./types/EventIcon"
-export * from "./types/FloatIcon"
-export * from "./types/IntegerIcon"
-export * from "./types/TextIcon"
+export * from "./types/concept-icon"
+export * from "./types/entity-icon"
+export * from "./types/event-icon"
+export * from "./types/float-icon"
+export * from "./types/integer-icon"
+export * from "./types/text-icon"
 
 
-export * from "./SchemaEntityIcon"
+export * from "./schema-entity-icon"
