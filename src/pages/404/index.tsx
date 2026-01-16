b3a1a6ae8d86d6d604e5222fcb186c7b47829659
thundax-lyp <731378491@qq.com>
2026-01-16 16:31:51 +0800

    prettier

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,6 +1,6 @@
 import styles from './index.module.less'
 
-function Page404 (){
+function Page404() {
     return (
         <div className={styles.page404}>
             <img className={styles.page404_icon} src="https://u1.dl0.cn/images/l2963j.png" alt="" srcSet="" />
@@ -8,9 +8,11 @@ function Page404 (){
                 <h3>抱歉，您访问的页面不存在!</h3>
                 <p>请确认链接地址是否正确后重新尝试</p>
             </div>
-            <div className={styles.page404_button} onClick={()=>{
-                location.href = '/'
-            }}
+            <div
+                className={styles.page404_button}
+                onClick={() => {
+                    location.href = '/'
+                }}
             >
                 回到首页
             </div>
@@ -18,4 +20,4 @@ function Page404 (){
     )
 }
 
-export default Page404;
+export default Page404
